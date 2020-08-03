c086165ae16bbaad4b2c0c0594843efc51d20e16
Yingshi Zhang <yingshizhang7@gmail.com>
2020-08-02 20:51:35 -0700

    Add connection to db

---
+++ db.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+#establishing the connection
+conn = mysql.connector.connect(
+   user='root', password='password', host='127.0.0.1', database='mydb')
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+# Preparing SQL query to INSERT a record into the database.
+sql = """INSERT INTO reservation(
+   table_id, timestamp, reserved)
+   VALUES ('1', '2020-08-02 20:27:59', 'True')"""
+
+try:
+   # Executing the SQL command
+   cursor.execute(sql)
+
+   # Commit your changes in the database
+   conn.commit()
+
+except:
+   # Rolling back in case of error
+   conn.rollback()
+
+# Closing the connection
+conn.close()
